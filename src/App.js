c28c9f53ffd99e3f064597a3b871cd4ac34f9491
Kyle <121882522+KTC96@users.noreply.github.com>
2024-08-01 11:04:35 +0100

    refactor code to use props

--- src/App.js
+++ src/App.js
@@ -5,6 +5,75 @@ import BusinessList from './components/BusinessList';
 import SearchBar from './components/SearchBar';
 
 
+export const businesses = [
+  {
+    image: 'https://patch.com/img/cdn20/users/24423246/20231110/123216/styles/patch_image/public/unnamed-54___10123038855.jpg?width=1200',
+    name: 'Restaurant Two',
+    address: '456 Side St',
+    city: 'Othertown',
+    state: 'TX',
+    zipcode: '75001',
+    category: 'Mexican',
+    rating: 4.0,
+    reviewCount: 85
+  },
+  {
+    image: 'https://patch.com/img/cdn20/users/24423246/20231110/123216/styles/patch_image/public/unnamed-54___10123038855.jpg?width=1200',
+    name: 'Restaurant T',
+    address: '456 Side St',
+    city: 'Othertown',
+    state: 'TX',
+    zipcode: '75001',
+    category: 'Mexican',
+    rating: 4.0,
+    reviewCount: 85
+  },
+  {
+    image: 'https://patch.com/img/cdn20/users/24423246/20231110/123216/styles/patch_image/public/unnamed-54___10123038855.jpg?width=1200',
+    name: 'Restaurant Two',
+    address: '456 Side St',
+    city: 'Othertown',
+    state: 'TX',
+    zipcode: '75001',
+    category: 'Mexican',
+    rating: 4.0,
+    reviewCount: 85
+  },
+  {
+    image: 'https://patch.com/img/cdn20/users/24423246/20231110/123216/styles/patch_image/public/unnamed-54___10123038855.jpg?width=1200',
+    name: 'Restaurant Two',
+    address: '456 Side St',
+    city: 'Othertown',
+    state: 'TX',
+    zipcode: '75001',
+    category: 'Mexican',
+    rating: 4.0,
+    reviewCount: 85
+  },
+  {
+    image: 'https://patch.com/img/cdn20/users/24423246/20231110/123216/styles/patch_image/public/unnamed-54___10123038855.jpg?width=1200',
+    name: 'Restaurant Two',
+    address: '456 Side St',
+    city: 'Othertown',
+    state: 'TX',
+    zipcode: '75001',
+    category: 'Mexican',
+    rating: 4.0,
+    reviewCount: 85
+  },
+  {
+    image: 'https://patch.com/img/cdn20/users/24423246/20231110/123216/styles/patch_image/public/unnamed-54___10123038855.jpg?width=1200',
+    name: 'Restaurant Two',
+    address: '456 Side St',
+    city: 'Othertown',
+    state: 'TX',
+    zipcode: '75001',
+    category: 'Mexican',
+    rating: 4.0,
+    reviewCount: 85
+  }
+];
+
 function App() {
   return (
     <div className="App">
@@ -12,7 +81,7 @@ function App() {
         <h1>FindMyTable</h1>
       </div>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
--- src/components/Business.js
+++ src/components/Business.js
@@ -1,23 +1,24 @@
 import React from 'react';
-const Business = ({ business }) => {
+
+const Business = (props) => {
     return (
       <div className="business">
         <div className="image-container">
-          <img src={business.image} alt={business.name} />
+          <img src={props.business.image} alt={props.business.name} />
         </div>
         
         <div className="business-info">
           <div class="address">
-          <h2>{business.name}</h2>
-            <p>{business.address}</p>
-            <p>{business.city}</p>
-            <p>{`${business.state} ${business.zipcode}`}</p>
+          <h2>{props.business.name}</h2>
+            <p>{props.business.address}</p>
+            <p>{props.business.city}</p>
+            <p>{`${props.business.state} ${props.business.zipcode}`}</p>
           </div>
           <div class="additional-info">
-            <p id="category"><strong>{business.category}</strong></p>
+            <p id="category"><strong>{props.business.category}</strong></p>
             <div className="rating">
-              <p><strong>{business.rating} stars</strong></p>
-              <p>{`${business.reviewCount} reviews`}</p>
+              <p><strong>{props.business.rating} stars</strong></p>
+              <p>{`${props.business.reviewCount} reviews`}</p>
             </div>
           </div>
         </div>
